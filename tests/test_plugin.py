a35e2f87eac81b63c711810f7215a2375ff4121a
shreyanshs7 <shreyanshss7@gmail.com>
2024-01-08 22:00:36 +0530

    added tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -9,6 +9,38 @@ class TestPlugin:
     file_name: str = "plugin"
     flake8_file_path: str = ".flake8"
 
+    def test_when_allowed_imports_config_is_not_present(self):
+        with open(self.flake8_file_path, 'w') as file:
+            file.write(
+                """
+                [flake8]
+                denied_imports_config={'plugin': ['tests'], 'tests': ['plugin']}
+                """
+            )
+        import_node = ast.Import(names=[ast.alias(name='abcd', asname=None)], lineno=1, col_offset=1)
+        tree = ast.Module(body=[import_node])
+        plugin = Plugin(tree=tree, filename=self.file_name)
+        with pytest.raises(ValueError):
+            next(plugin.run())
+
+        os.remove(self.flake8_file_path)
+
+    def test_when_denied_imports_config_is_not_present(self):
+        with open(self.flake8_file_path, 'w') as file:
+            file.write(
+                """
+                [flake8]
+                allowed_imports_config={'plugin': ['tests'], 'tests': ['plugin']}
+                """
+            )
+        import_node = ast.Import(names=[ast.alias(name='abcd', asname=None)], lineno=1, col_offset=1)
+        tree = ast.Module(body=[import_node])
+        plugin = Plugin(tree=tree, filename=self.file_name)
+        with pytest.raises(ValueError):
+            next(plugin.run())
+
+        os.remove(self.flake8_file_path)
+
     def test_when_correct_import_is_present(self):
         with open(self.flake8_file_path, 'w') as file:
             file.write(
